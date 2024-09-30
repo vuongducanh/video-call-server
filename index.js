d1768e1a021b2b894ddd66e6532b767976bac7ee
anhvd001 <anhvd@nal.vn>
2024-09-30 17:27:34 +0700

    remove socket io

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ var express = require("express");
 
 var app = (module.exports = express.createServer());
 
-var io = require("socket.io")(app);
+// var io = require("socket.io")(app);
 
-io.on("connection", function (socket) {
-  io.sockets.emit(
-    "user-joined",
-    socket.id,
-    io.engine.clientsCount,
-    Object.keys(io.sockets.clients().sockets)
-  );
+// io.on("connection", function (socket) {
+//   io.sockets.emit(
+//     "user-joined",
+//     socket.id,
+//     io.engine.clientsCount,
+//     Object.keys(io.sockets.clients().sockets)
+//   );
 
-  socket.on("signal", (toId, message) => {
-    io.to(toId).emit("signal", socket.id, message);
-  });
+//   socket.on("signal", (toId, message) => {
+//     io.to(toId).emit("signal", socket.id, message);
+//   });
 
-  socket.on("message", function (data) {
-    io.sockets.emit("broadcast-message", socket.id, data);
-  });
+//   socket.on("message", function (data) {
+//     io.sockets.emit("broadcast-message", socket.id, data);
+//   });
 
-  socket.on("disconnect", function () {
-    io.sockets.emit("user-left", socket.id);
-  });
-});
+//   socket.on("disconnect", function () {
+//     io.sockets.emit("user-left", socket.id);
+//   });
+// });
 
 app.listen(3000, function () {
   console.log(
