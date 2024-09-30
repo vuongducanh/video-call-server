6b2f9ff95d77c981570eabde84bf44e4ca7ffb1e
anhvd001 <anhvd@nal.vn>
2024-09-30 20:18:47 +0700

    update code 1

--- index.js
+++ index.js
@@ -37,46 +37,32 @@
 //   res.send("Work ok");
 // });
 
-// api/socket.js
+
 const express = require("express");
-const { createServer } = require("http");
+const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-const server = createServer(app);
+const server = http.createServer(app);
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  io.sockets.emit(
-    "user-joined",
-    socket.id,
-    io.engine.clientsCount,
-    Object.keys(io.sockets.sockets)
-  );
-
-  socket.on("signal", (toId, message) => {
-    io.to(toId).emit("signal", socket.id, message);
-  });
+  console.log(`User connected: ${socket.id}`);
 
   socket.on("message", (data) => {
-    io.sockets.emit("broadcast-message", socket.id, data);
+    io.emit("message", data);
   });
 
   socket.on("disconnect", () => {
-    io.sockets.emit("user-left", socket.id);
+    console.log(`User disconnected: ${socket.id}`);
   });
 });
 
-// Cung cấp endpoint cho Vercel
 app.get("/", (req, res) => {
-  res.send("Work ok");
+  res.send("WebSocket server is running!");
 });
 
-// Xuất server cho Vercel
-module.exports = (req, res) => {
-  if (req.method === 'GET') {
-    return app(req, res);
-  }
-  // Đối với WebSocket, bạn cần xử lý kết nối riêng
-  return server(req, res);
-};
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
